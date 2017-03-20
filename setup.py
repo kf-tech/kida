fb3d3abdf4ae46b47d1c12682a619c30ee8a9126
kevenli <pbleester@gmail.com>
2017-03-20 16:27:14 +0800

    exclude tests when packaging

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name = 'PyDB',
     version = PyDB.__version__,
-    packages = find_packages(),
-    include_package_data = True,
+    packages = find_packages(exclude=('tests', 'tests.*')),
+    include_package_data=True,
 )
